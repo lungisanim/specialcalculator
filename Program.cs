ab166daf742ef4e59efa63423daccbd56adfc085
dragonfly <lm.nhlengethwa@outlook.com>
2023-04-25 10:52:54 +0200

    Special Mathematics Calcualator

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpecialCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> testNumbers = new List<string> { "" , "1" , "1,2" , "1\n2,3" , "//;\n1;2" , "2,1001" , "-1,2,-3,4" };
+            foreach (var test in testNumbers)
+            {
+                Console.WriteLine(Add(test));
+            }
+
+        }
+
+        public static int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            char[] delimiters = GetDelimiters(ref numbers);
+            string[] numArray = numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            List<int> negativeNumbers = new List<int>();
+
+            int sum = 0;
+            foreach (var num in numArray)
+            {
+                int parsedNum;
+                if (int.TryParse(num, out parsedNum))
+                {
+                    if (parsedNum < 0)
+                        negativeNumbers.Add(parsedNum);
+
+                    sum += parsedNum;
+                }
+            }
+
+            if (negativeNumbers.Count > 0)
+                throw new Exception("Negatives not allowed: " + string.Join(", ", negativeNumbers));
+            
+            return sum;
+        }
+
+        static char[] GetDelimiters(ref string numbers)
+        {
+            char[] delimiters = new char[] { ',', '\n' }; 
+
+            if (numbers.StartsWith("//"))
+            {
+                int delimiterIndex = numbers.IndexOf("\n");
+                string delimiterString = numbers.Substring(2, delimiterIndex - 2);
+                delimiters = new char[] { Convert.ToChar(delimiterString) };
+                numbers = numbers.Substring(delimiterIndex + 1);
+            }
+
+            return delimiters;
+        }
+    }
+}
---
+++ StringCalculator.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecialCalculator;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void Test_Add_EmptyString_ReturnsZero()
+        {
+            // Arrange
+            string input = "";
+
+            // Act
+            int result = Program.Add(input);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test_Add_SingleNumber_ReturnsNumber()
+        {
+            // Arrange
+            string input = "1";
+
+            // Act
+            int result = Program.Add(input);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test_Add_TwoNumbers_ReturnsSum()
+        {
+            // Arrange
+            string input = "1,2";
+
+            // Act
+            int result = Program.Add(input);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test_Add_NewLinesBetweenNumbers_ReturnsSum()
+        {
+            // Arrange
+            string input = "1\n2,3";
+
+            // Act
+            int result = Program.Add(input);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Test_Add_ChangeDelimiter_ReturnsSum()
+        {
+            // Arrange
+            string input = "//;\n1;2";
+
+            // Act
+            int result = Program.Add(input);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test_Add_IgnoreNumbersGreaterThan1000_ReturnsSum()
+        {
+            // Arrange
+            string input = "2,1001";
+
+            // Act
+            int result = Program.Add(input);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception), "Negatives not allowed: -1, -3")]
+        public void Test_Add_NegativeNumbers_ThrowsException()
+        {
+            // Arrange
+            string input = "-1,2,-3,4";
+
+            // Act and Assert
+            int result = Program.Add(input);
+        }
+    }
+}
